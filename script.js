13f826147b91273295c5141f6dea9b25209084c0
sketch <54027485+suketti@users.noreply.github.com>
2024-04-16 13:51:11 +0200

    Add both way questions

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let thesis3 = {
-    "Title": "Ipari forradalom", 
-    "Questions" : [""],
-    "Answers": [""] 
+    "title": "Ipari forradalom", 
+    "events" : ["I. ipari forradalom", "II. ipari forradalom", "gőzgép(James Watt)"],
+    "dates": ["1750 - 19.század közepe", "1871 - 1914"], 
+    "images": ["elso_ipari_forradalom#3"]
 }
 
 let theses = {
@@ -9,32 +10,60 @@ let theses = {
     "thesis3" : thesis3
 }
 
-function getRandomAnswer() {
+function getRandomDate() {
     let randomNum = Math.floor(Math.random() * theses.validNumbers.length)
     let thesisString = "thesis" + String(theses.validNumbers[randomNum])
-    let randomAnswer = theses[thesisString].Answers[Math.floor(Math.random() * theses[thesisString].Answers.length)]
-    return randomAnswer; 
+    let randomDate = theses[thesisString].dates[Math.floor(Math.random() * theses[thesisString].dates.length)]
+    return randomDate; 
 }
 
+function getRandomEvent() {
+    let randomNum = Math.floor(Math.random() * theses.validNumbers.length)
+    let thesisString = "thesis" + String(theses.validNumbers[randomNum])
+    let randomEvent = theses[thesisString].events[Math.floor(Math.random() * theses[thesisString].events.length)]
+    return randomEvent;
+}
 
-function getRandomQuestionFromThesis(thesisNumber) {
+function getRandomEventFromThesis(thesisNumber) {
+    console.log("WAAAAS")
     if (theses.validNumbers.includes(thesisNumber)) {
+        console.log("IST")
         let thesisString = "thesis" + String(thesisNumber)
-        let randomNumber = Math.floor(Math.random() * theses[thesisString].Questions.length)
-        let question = theses[thesisString].Questions[randomNumber]
-        let correctAnswer = theses[thesisString].Answers[randomNumber]
+        let randomNumber = Math.floor(Math.random() * theses[thesisString].events.length)
+        let question = theses[thesisString].events[randomNumber]
+        let correctDate = theses[thesisString].dates[randomNumber]
         let badResponses = []
 
         for (let i = 0; i < 3; i++) {
-            let answ = getRandomAnswer();
-            while (answ == correctAnswer) {
-                answ = getRandomAnswer()
+            let answ = getRandomDate();
+            while (answ != correctDate) {
+                answ = getRandomDate()
             }
             badResponses.push(answ)
         }
     
-        let response = [question, [correctAnswer, ...badResponses]]
+        let response = [question, [correctDate, ...badResponses]]
         console.log(response)
     }   
 }
 
+function getRandomDateFromThesis(thesisNumber) {
+     if (theses.validNumbers.includes(thesisNumber)) {
+         let thesisString = "thesis" + String(thesisNumber)
+         let randomNumber = Math.floor(Math.random() * theses[thesisString].events.length)
+         let date = theses[thesisString].dates[randomNumber]
+         let correctdate = theses[thesisString].events[randomNumber]
+         let badResponses = []
+
+         for (let i = 0; i < 3; i++) {
+             let answ = getRandomEvent();
+             while (answ != correctEvent) {
+                 answ = getRandomEvent()
+             }
+             badResponses.push(answ)
+         }
+    
+         let response = [date, [correctdate, ...badResponses]]
+         console.log(response)
+     }    
+}
